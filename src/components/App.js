89f4825c8896d4af408a6f04f8853738faab64d4
John Verdone <jminterwebs@gmail.com>
2019-06-04 22:24:57 -0400

    added sample components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,66 @@
 import React from 'react';
 
-import 'components/App.css';
+import Heading from 'components/typography/Heading';
+import Label from 'components/typography/Label';
+import Title from 'components/typography/Title';
+
+import Avatar from 'components/avatar/Avatar';
+
+import 'styles/scss/global.scss';
+import 'styles/scss/localStyles.scss';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <Heading text="Color Boxes" size="x-large" />
+      <div className="color-boxes">
+        <div className="color-box primary">
+          <Label text="primary" labelColor="white" />
+        </div>
+
+        <div className="color-box success">
+          <Label text="success" labelColor="white" />
+        </div>
+        <div className="color-box info">
+          <Label label="info" labelColor="white" />
+        </div>
+        <div className="color-box warning">
+          <Label text="warning" labelColor="white" />
+        </div>
+        <div className="color-box danger">
+          <Label text="danger" labelColor="white" />
+        </div>
+        <div className="color-box default">
+          <Label text="default" labelColor="white" />
+        </div>
+      </div>
+      <br />
+      <Heading text="Typography" size="x-large" />
+      <br />
+      <Heading text="Headings" size="medium" />
+      <Heading text="Heading 1" size="x-large" color="primary" />
+      <Heading text="Heading 1" size="large" />
+      <Heading text="Heading 1" size="medium" />
+      <Heading text="Heading 1" size="small" />
+      <br />
+      <Heading text="Label" size="medium" />
+
+      <Label text="Label" weight="400" />
+      <Label text="Label" weight="bold" />
+
+      <br />
+      <Heading text="Title" size="medium" />
+      <br />
+      <Title text="Title Text" color="danger" />
+
+      <Avatar
+        imageSrc="https://picsum.photos/300"
+        label="John Verdone"
+        subLabel="Web Developer"
+        initials="JV"
+      />
+
+      <br />
     </div>
   );
 }
