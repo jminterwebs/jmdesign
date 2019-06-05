581b4c6c19406ce26e8115475e71936d760e0914
John Verdone <jminterwebs@gmail.com>
2019-06-04 22:22:56 -0400

    removed redux

--- src/index.js
+++ src/index.js
@@ -3,25 +3,10 @@ import 'index.css';
 // React imports
 import React from 'react';
 import { render } from 'react-dom';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
 
 // Component, reducer, actions import
 import App from 'components/App';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-// http://extension.remotedev.io/
-const reduxDevtoolsExtension =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-render(
-  <Provider store={createStoreWithMiddleware(reduxDevtoolsExtension)}>
-    <App />
-  </Provider>,
-
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
 // registerServiceWorker()
