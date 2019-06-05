dbeb0dec54c84520192bc94a20982bb5e8131e13
John Verdone <jminterwebs@gmail.com>
2019-06-04 22:27:37 -0400

    avatar component

---
+++ src/components/avatar/Avatar.js
@@ -0,0 +1,41 @@
+// Core React
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Thumbnail from '../image/Thumbnail';
+import Label from '../typography/Label';
+import SubLabel from '../typography/SubLabel';
+
+import './avatar.scss';
+
+export default function Avatar(props) {
+  const { imageSrc, label, subLabel, initials } = props;
+
+  const avatarPicture = imageSrc ? (
+    <Thumbnail imageSrc={imageSrc} rounded size="small" />
+  ) : (
+    <Label label={initials} />
+  );
+
+  return (
+    <div className="avatar">
+      {avatarPicture}
+      <div className="avatar-text">
+        <Label label={label} />
+        <SubLabel subLabel={subLabel} />
+      </div>
+    </div>
+  );
+}
+
+Avatar.propTypes = {
+  imageSrc: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  subLabel: PropTypes.string,
+  initials: PropTypes.string.isRequired
+};
+
+Avatar.defaultProps = {
+  imageSrc: '',
+  subLabel: ''
+};
