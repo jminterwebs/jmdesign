0d525d3642afce54b9ac91684153fd1245eca733
John Verdone <jmverdone@gmail.com>
2019-06-20 21:03:25 -0400

    breadcrumb stubbed  out

---
+++ src/components/breadcrumb/Breadcrumb.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Label from '../typography/Label';
+
+function Breadcrumb(props) {
+  const { children } = props;
+
+  return <nav className="breadcrumb-nav">{children}</nav>;
+}
+
+function Home(props) {
+  const { name, link } = props;
+
+  return (
+    <li className="home-crumb">
+      <a href={link}>
+        <Label label={name} />
+      </a>
+    </li>
+  );
+}
+function PreviousLink(props) {
+  const { name, link } = props;
+
+  return (
+    <li className="home-crumb">
+      <a href={link}>
+        <Label label={name} />
+      </a>
+    </li>
+  );
+}
+
+function Current(props) {
+  const { name } = props;
+
+  return (
+    <li className="home-crumb">
+      <Label label={name} />
+    </li>
+  );
+}
+
+Breadcrumb.Home = Home;
+Breadcrumb.History = PreviousLink;
+Breadcrumb.Current = Current;
+
+export default Breadcrumb;
