dfb7aec2cd603bf2586fbcced2ea7c8c7a1bc05f
John Verdone <jmverdone@gmail.com>
2019-07-21 21:50:11 -0400

    added index file

---
+++ src/components/index.js
@@ -0,0 +1,37 @@
+import { default as Avatar } from './avatar/Avatar';
+import { default as Badge } from './badge/Badge';
+import { default as Breadcrumb } from './breadcrumb/Breadcrumb';
+import { default as CalloutBanner } from './calloutBanner/CalloutBanner';
+import { default as Card } from './card/Card';
+import { default as DefinitionTable } from './definitionTable/DefinitionTable';
+import { default as HorizontalTab } from './horizontalTab/HorizontalTabs';
+import { default as Image } from './image/Image';
+import { default as Thumbnail } from './image/Thumbnail';
+import { default as List } from './list/List';
+import { default as Toggle } from './toggle/Toggle';
+import { default as BodyText } from './typography/BodyText';
+import { default as Heading } from './typography/Heading';
+import { default as Label } from './typography/Label';
+import { default as SubLabel } from './typography/SubLabel';
+import { default as Title } from './typography/Title';
+import { default as VerticalTabs } from './verticalTabs/VerticalTabs';
+
+export {
+  Avatar,
+  Badge,
+  Breadcrumb,
+  CalloutBanner,
+  Card,
+  DefinitionTable,
+  HorizontalTab,
+  Image,
+  Thumbnail,
+  List,
+  Toggle,
+  BodyText,
+  Heading,
+  Label,
+  SubLabel,
+  Title,
+  VerticalTabs
+};
